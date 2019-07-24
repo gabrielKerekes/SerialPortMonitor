05a6bd49e2b8ad50bc5ea51e9128155d8ce9f398
GabrielK <gabriel.kerekes@taphome.com>
2019-07-24 09:02:17 +0200

    Removed comments and stuff

--- SerialPortMonitor/Program.cs
+++ SerialPortMonitor/Program.cs
@@ -1,68 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO.Ports;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace SerialPortMonitor
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Arguments: port, baudrate, parity.");
+            Console.WriteLine("Example: mono SerialPortMonitor.exe /dev/ttyBUS0 19200 None/Even/Odd/Mark/Space");
+            Console.WriteLine();
+
             var port = new SerialPort();
-            var portName = args.Length > 0 ? args[0] : "COM7";
-            Console.WriteLine(portName);
+            var portName = args.Length > 0 ? args[0] : "/dev/ttyBUS0";
+            var baudRate = args.Length > 1 ? int.Parse(args[1]) : 9600;
+            var parity = args.Length > 2 ? (Parity) Enum.Parse(typeof(Parity), args[2]) : Parity.None;
+
+            Console.WriteLine($"Port: {portName}");
+            Console.WriteLine($"Baudrate: {baudRate}");
+            Console.WriteLine($"Parity: {parity}");
+
             port.PortName = portName;
-            port.BaudRate = 9600;
-            port.Parity = Parity.None;
-
-            //port.DataReceived += (sender, eventArgs) =>
-            //{
-            //    var sp = (SerialPort)sender;
-
-            //    var bytesToRead = sp.BytesToRead;
-            //    var bytes = new byte[bytesToRead];
-            //    var indata = sp.Read(bytes, 0, bytesToRead);
-
-            //    if (bytes.Length > 2 && bytes[1] == 6)
-            //    {
-            //        Console.Write(ByteArrayToString(bytes));
-            //        Console.WriteLine();
-            //    }
-            //};
+            port.BaudRate = baudRate;
+            port.Parity = parity;
+            port.StopBits = StopBits.One;
 
             port.Open();
 
             while (true)
             {
-                //var bytesToRead = port.BytesToRead;
-                //var bytes = new byte[bytesToRead];
-                //port.Read(bytes, 0, bytesToRead);
-
-                //Console.WriteLine(ByteArrayToString(bytes));
-
-                //Thread.Sleep(500);
-                //port.Write("ASDASD");
-                //Thread.Sleep(500);
                 var bytesToRead = port.BytesToRead;
                 var bytes = new byte[bytesToRead];
-                var indata = port.Read(bytes, 0, bytesToRead);
+                var numberOfBytes = port.Read(bytes, 0, bytesToRead);
 
-                //if (bytes.Length > 2 && bytes[1] == 6)
-                //{
+                if (bytes.Length > 0)
+                {
                     Console.Write(ByteArrayToString(bytes));
                     Console.WriteLine();
-                //}
+                }
 
                 Thread.Sleep(500);
             }
         }
+
         public static string ByteArrayToString(byte[] ba)
         {
-            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            var hex = new StringBuilder(ba.Length * 2);
             foreach (byte b in ba)
                 hex.AppendFormat("{0:x2} ", b);
             return hex.ToString();
